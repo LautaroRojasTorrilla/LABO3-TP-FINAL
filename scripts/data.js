e772adfc317b7c2d50d77f37a6a969acd36af59a
LautaroRojasTorrilla <lautaro.rojas.torrilla@gmail.com>
2024-07-08 07:49:35 -0300

    db en archivo

---
+++ scripts/data.js
@@ -0,0 +1,98 @@
+const masNosotrosData = [
+    {
+        img: "./images/icono1.svg",
+        title: "Seguridad",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo officiis ducimus facere qui quos placeat, quasi maxime sunt? Iure alias blanditiis quos impedit at velit doloribus nihil quis est fugiat?"
+    },
+    {
+        img: "./images/icono2.svg",
+        title: "El Mejor precio",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo officiis ducimus facere qui quos placeat, quasi maxime sunt? Iure alias blanditiis quos impedit at velit doloribus nihil quis est fugiat?"
+    },
+    {
+        img: "./images/icono3.svg",
+        title: "A tiempo",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo officiis ducimus facere qui quos placeat, quasi maxime sunt? Iure alias blanditiis quos impedit at velit doloribus nihil quis est fugiat?"
+    }
+];
+
+const casasData = [
+    {
+        img: "./images/anuncio1.jpg",
+        title: "Casa de lujo en el lago",
+        text: "Casa en el lago con excelente vista, acabados de lujo a un excelente precio",
+        price: "u$s350.000",
+        features: [
+            { img: "./images/icono_wc.svg", text: "3" },
+            { img: "./images/icono_estacionamiento.svg", text: "3" },
+            { img: "./images/icono_dormitorio.svg", text: "4" }
+        ]
+    },
+    {
+        img: "./images/anuncio2.jpg",
+        title: "Casa con terminados de lujo",
+        text: "Casa con diseño moderno, así como tecnología inteligente y amueblada",
+        price: "u$s275.000",
+        features: [
+            { img: "./images/icono_wc.svg", text: "4" },
+            { img: "./images/icono_estacionamiento.svg", text: "5" },
+            { img: "./images/icono_dormitorio.svg", text: "4" }
+        ]
+    },
+    {
+        img: "./images/anuncio3.jpg",
+        title: "Casa con pileta",
+        text: "Casa con pileta y acabados de lujo en la ciudad, excelente oportunidad",
+        price: "u$s250.000",
+        features: [
+            { img: "./images/icono_wc.svg", text: "3" },
+            { img: "./images/icono_estacionamiento.svg", text: "3" },
+            { img: "./images/icono_dormitorio.svg", text: "4" }
+        ]
+    }
+];
+
+const blogData = [
+    {
+        img: "./images/blog1.jpg",
+        title: "Terraza en el techo de tu casa",
+        date: "20/10/2019",
+        author: "Admin",
+        text: "Consejos para construir una terraza en el techo de tu casa, con los mejores materiales y ahorrando dinero."
+    },
+    {
+        img: "./images/blog2.jpg",
+        title: "Guía para la decoración de tu hogar",
+        date: "20/10/2019",
+        author: "Admin",
+        text: "Maximiza el espacio en tu hogar con esta guía, aprende a combinar muebles y colores para darle vida a tu espacio."
+    }
+];
+
+const commentsData = [
+    {
+        text: "El personal se comportó de una excelente forma. Muy buena atención y la casa que me ofrecieron cumple con todas mis expectativas.",
+        author: "Juan Perez"
+    },
+    {
+        text: "Muy buen servicio y atención al cliente. Estoy muy satisfecha con la compra de mi nueva casa.",
+        author: "Maria Garcia"
+    },
+    {
+        text: "La mejor experiencia de compra de una propiedad que he tenido. Altamente recomendable.",
+        author: "Carlos Lopez"
+    },
+    {
+        text: "Excelente trato y profesionalismo. Me ayudaron en todo el proceso de compra.",
+        author: "Ana Torres"
+    },
+    {
+        text: "Atención personalizada y muy efectiva. Gracias por ayudarme a encontrar la casa de mis sueños.",
+        author: "Luis Rodriguez"
+    },
+    {
+        text: "Servicio de primera calidad. Estoy muy contenta con mi nueva casa.",
+        author: "Sofia Martinez"
+    }
+];
+
